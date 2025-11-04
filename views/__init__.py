bc70173b6f47947e7f4f89feed0611bdca7f7940
Đặng Vũ Hưng <88934176+singed2905@users.noreply.github.com>
2025-11-04 16:45:35 +0700

    Add views package __init__.py

---
+++ views/__init__.py
@@ -0,0 +1,9 @@
+# Views package for ConvertKeylogApp
+# Contains all user interface components
+
+from .main_view import MainView
+from .geometry_view import GeometryView
+from .equation_view import EquationView
+from .polynomial_equation_view import PolynomialEquationView
+
+__all__ = ['MainView', 'GeometryView', 'EquationView', 'PolynomialEquationView']
